0f248f17f9574bd994f46a47d91c158498df87b8
TJ Taylor <dugancathal@gmail.com>
2015-11-27 17:48:22 -0700

    Use command line args!

--- app.py
+++ app.py
@@ -1,11 +1,25 @@
 import sys
 from builds import circleci
 from neopixels.pixel_strip import PixelStrip
+import argparse
 
+parser = argparse.ArgumentParser(description='Monitor your build!')
+parser.add_argument('--project', action='store', required=True,
+                   help='the project name/url/whatever your build script needs')
+parser.add_argument('--token', action='store', required=True,
+                   help='the password/secret/what-have-you for logging into your build system')
+parser.add_argument('--filler', action='store', default='cycle',
+                    choices=['cycle', 'multitwinkle', 'twinkle', 'gradient'],
+                    help='what to do when not showing you your build state')
+parser.add_argument('--gradient', action='store', default='hanukkah',
+                    help='the color scheme to show for the gradient filler')
+parser.add_argument('--poll-frequency', action='store', default='30',
+                    help='number of seconds to wait between polls')
+
+args = parser.parse_args()
 if __name__ == "__main__":
     strip = PixelStrip()
-    circle_token = sys.argv[1]
-    fetcher = circleci.BuildFetcher("TruHearing/echo", circle_token)
+    fetcher = circleci.BuildFetcher(args.project, args.token)
 
     for index, build in enumerate(fetcher.builds()):
         strip.color_wipe(build.to_color())
