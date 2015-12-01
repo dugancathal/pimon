049e83a999d28a8d179c1e0c9c5d1673d1c419d8
TJ Taylor <dugancathal@gmail.com>
2015-11-30 22:35:25 -0700

    Add example script for multi-swipe

---
+++ bin/examples/multi-swipe.py
@@ -0,0 +1,14 @@
+import time
+
+from neopixels.pixel_strip import PixelStrip
+from neopixels.multi_swipe import MultiSwipe
+from utils import schemes, colors
+
+if __name__ == "__main__":
+    strip = PixelStrip()
+    swipe = MultiSwipe(strip, [colors.green, colors.yellow, colors.red])
+
+    while True:
+        swipe.step()
+        print "Sleeping..."
+        time.sleep(1)
